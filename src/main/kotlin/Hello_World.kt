634063084e8cea7d0fda2129e6dd21953e02b27c
Shubham  Saurav <shubhamsinha186@gmail.com>
2022-09-11 23:07:50 +0530

    Variables files added

---
+++ src/main/kotlin/Hello_World.kt
@@ -0,0 +1,4 @@
+fun main() {
+    println("Hello world!")
+    println("Welcome to BuildWithShubham")
+}
---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var age = 100
+    println(age)
+    var per = 67.23
+    println(per)
+    var myName = "Shubham"
+    println(myName)
+
+    myName = "Shubham Saurav"
+    println(myName)
+    age = 200
+    per = 99.20
+    println(age)
+    println(per)
+
+
+    val num = 10
+    println(num)
+
+
+
+}

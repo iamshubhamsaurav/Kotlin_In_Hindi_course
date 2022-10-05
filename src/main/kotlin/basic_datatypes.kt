e42e66fceb80f42cc0fc4277ff3313081a27778e
Shubham  Saurav <shubhamsinha186@gmail.com>
2022-10-05 11:22:48 +0530

    Type inference and explicit declaration

--- src/main/kotlin/basic_datatypes.kt
+++ src/main/kotlin/basic_datatypes.kt
@@ -9,17 +9,29 @@
 
 
 fun main() {
-    val myInt = 78
+    val myInt = 99
     val myDouble = 879.342
     val myString = "Shubham"
     val myChar = 'a'
     val myBoolean = false
 
 
-    println(myInt)
-    println(myDouble)
-    println(myString)
-    println(myChar)
-    println(myBoolean)
+//    println(myInt)
+//    println(myDouble)
+//    println(myString)
+//    println(myChar)
+//    println(myBoolean)
+
+    var num: Int = 67
+    println(num)
+    val d2: Double = 93.23
+    val s2: String = "dfhuhu"
+    val c2: Char = 'a'
+    val b2: Boolean = false
+
+
+    val fullName: String
+    fullName = "shubham"
+
 
 }

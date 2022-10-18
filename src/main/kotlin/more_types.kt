a392a6dc4f893ccae13e8425443fff5fda834d90
Shubham  Saurav <shubhamsinha186@gmail.com>
2022-10-18 12:47:10 +0530

    More Data Types

---
+++ src/main/kotlin/more_types.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val myByte: Byte = 120
+    println(myByte)
+    val myShort: Short = -20000
+    println(myShort)
+    val myLong = 784
+    println(myLong)
+    val myFloat = 2.3F
+    println(myFloat)
+}

b530d7f4ba7015a7105528a99baebfb1a6148cb7
Shubham  Saurav <shubhamsinha186@gmail.com>
2022-10-29 18:04:16 +0530

    Comparison Operator file added

---
+++ src/main/kotlin/comparison_operator.kt
@@ -0,0 +1,16 @@
+fun main() {
+// Comparison operator = Used to compare two values
+//    >	greater than
+//    <	less than
+//    >= greater than or equal to
+//    <= less than or equal to
+//    == equal to
+//    != not equal to
+    val a = 5
+    val b = 5
+    if (a != b) {
+        println("a is not equal to b")
+    } else {
+        println("a is equal to b")
+    }
+}

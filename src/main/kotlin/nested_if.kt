aee6ea5b76442f5f75495147ed659a442c38fb9e
Shubham  Saurav <shubhamsinha186@gmail.com>
2022-11-16 18:36:13 +0530

    Nested If Statement

---
+++ src/main/kotlin/nested_if.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // Nested If Example
+    val age = 18
+
+    if(age >= 18) {
+        if(age == 18) {
+            println(" Age is equal to 18")
+        } else {
+            println("Age is greater than 18")
+        }
+    } else {
+        println("age is smaller than 18")
+    }
+}

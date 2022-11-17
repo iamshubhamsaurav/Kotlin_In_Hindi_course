64ffc75260d62b5d6a41580436254bec022e4743
Shubham  Saurav <shubhamsinha186@gmail.com>
2022-11-17 12:44:14 +0530

    When basics

---
+++ src/main/kotlin/when_basic_1.kt
@@ -0,0 +1,20 @@
+
+fun main() {
+
+    val day = 1
+
+    when(day) {
+     1 -> {
+         println("Sunday")
+         println("Line 2")
+         println("Line 3")
+     }
+     2 -> println("Monday")
+     3 -> println("Tuesday")
+     4 -> println("Wednesday")
+     5 -> println("Thursday")
+     6 -> println("Friday")
+     7 -> println("Saturday")
+        else -> println("Invalid day")
+    }
+}
